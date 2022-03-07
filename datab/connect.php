bcd06d3da7a2a326375f29a0531a14f81574c7e1
ale3x597 <90639588+ale3x597@users.noreply.github.com>
2022-03-07 02:47:39 -0500

    currently working on switching this over to try out PHP and integrate a database for the products that will be listed, so far I have almost finished with the Home page format. Will go over it once completely done to make it look better

---
+++ datab/connect.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "127.0.0.1";
+$username = "root";
+$password = "";
+
+try{
+    $handle= new PDO("mysql:host=$servername;dbname=myDB", $username,$password);
+
+    $handle->setAttribute(PDO::ATTR_ERRMODE, PDO:: ERRMODE_EXCEPTION);
+    echo "connected successfully";
+}
+catch(PDOException $e){
+    die("Something Went Wrong With DataBase. Please Try Again Later")
+}
+?>
--- index.php
+++ index.php
@@ -1,12 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Store Project</title>
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"/>
-    <link rel="stylesheet" href="styles.css">
+    <?php include 'head.php'; ?>
 </head>
 
 <body>
